eb338880fd4acfd51e9ba73bf10774117dba35f9
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-19 15:53:04 +0600

    data updated

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 
+// middleware
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 const formulaOne = require("./data/formulaOne.json");
 const train = require("./data/train.json");
+const truck = require("./data/car.json");
 
 app.get("/", (req, res) => {
   res.send("Speedy Toy is running...");
@@ -18,6 +20,9 @@ app.get("/formula-one", (req, res) => {
 });
 app.get("/train", (req, res) => {
   res.send(train);
+});
+app.get("/truck", (req, res) => {
+  res.send(truck);
 });
 
 app.listen(port, () => {
